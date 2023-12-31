4acb8eda32ca82214994c8277365cec3bb335a2d
Adam Hollett <mail@adamhollett.com>
2023-12-30 19:12:30 -0500

    Make most keys work

--- script.js
+++ script.js
@@ -17,10 +17,16 @@ document.querySelector("#key-7").addEventListener("click", () => updateScreenLow
 document.querySelector("#key-8").addEventListener("click", () => updateScreenLower("8"));
 document.querySelector("#key-9").addEventListener("click", () => updateScreenLower("9"));
 document.querySelector("#key-0").addEventListener("click", doKey0Press);
+document.querySelector("#key-00").addEventListener("click", doKey00Press);
 document.querySelector("#key-dot").addEventListener("click", doKeyDotPress);
 
 // Operator keys
 document.querySelector("#key-add").addEventListener("click", doKeyAddPress);
+document.querySelector("#key-subtract").addEventListener("click", doKeySubtractPress);
+document.querySelector("#key-multiply").addEventListener("click", doKeyMultiplyPress);
+document.querySelector("#key-divide").addEventListener("click", doKeyDividePress);
+document.querySelector("#key-equals").addEventListener("click", doKeyEqualsPress);
+document.querySelector("#key-clear").addEventListener("click", doKeyClearPress);
 
 function onlyZeroPresent() {
   return screen.lower[0] === "0" && screen.lower.length === 1 ? true : false;
@@ -29,6 +35,11 @@ function onlyZeroPresent() {
 function doKey0Press() {
   if (onlyZeroPresent()) return;
   updateScreenLower("0");
+}
+
+function doKey00Press() {
+  doKey0Press();
+  doKey0Press();
 }
 
 function doKeyDotPress() {
@@ -40,7 +51,82 @@ function doKeyAddPress() {
   screen.upper.push(screen.lower.join(""));
   screen.upper.push(" ");
   screen.upper.push("+");
+  screen.upper.push(" ");
+  screenUpper.innerHTML = screen.upper.join("");
 
+  screen.lower = ["0"];
+  screenLower.innerHTML = screen.lower.join("");
+}
+
+function doKeySubtractPress() {
+  screen.upper.push(screen.lower.join(""));
+  screen.upper.push(" ");
+  screen.upper.push("-");
+  screen.upper.push(" ");
+  screenUpper.innerHTML = screen.upper.join("");
+
+  screen.lower = ["0"];
+  screenLower.innerHTML = screen.lower.join("");
+}
+
+function doKeyMultiplyPress() {
+  screen.upper.push(screen.lower.join(""));
+  screen.upper.push(" ");
+  screen.upper.push("*");
+  screen.upper.push(" ");
+  screenUpper.innerHTML = screen.upper.join("");
+
+  screen.lower = ["0"];
+  screenLower.innerHTML = screen.lower.join("");
+}
+
+function doKeyDividePress() {
+  screen.upper.push(screen.lower.join(""));
+  screen.upper.push(" ");
+  screen.upper.push("/");
+  screen.upper.push(" ");
+  screenUpper.innerHTML = screen.upper.join("");
+
+  screen.lower = ["0"];
+  screenLower.innerHTML = screen.lower.join("");
+}
+
+function doKeyEqualsPress() {
+  let operation = screen.upper.concat(screen.lower).join("");
+
+  operation = operation.replace(/\W/, "");
+
+  const operator = operation.match(/[\+\-\*\/]/g).pop();
+
+  const lhs = Number(operation.split(operator)[0]);
+  const rhs = Number(operation.split(operator)[1]);
+  let result;
+
+  switch (operator) {
+    case "+":
+      result = lhs + rhs;
+      break;
+    case "-":
+      result = lhs - rhs;
+      break;
+    case "*":
+      result = lhs * rhs;
+      break;
+    case "/":
+      result = lhs / rhs;
+      break;
+  }
+
+  // TODO: Prevent integer operations from appending ".00"
+  screen.lower = [];
+  screenLower.innerHTML = result.toFixed(2);
+
+  screen.upper = [];
+  screenUpper.innerHTML = "";
+}
+
+function doKeyClearPress() {
+  screen.upper = []
   screenUpper.innerHTML = screen.upper.join("");
 
   screen.lower = ["0"];
